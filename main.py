35ee466108c7dffd282ecb584adb70ab599f2101
Solan <solan@eternalevil.de>
2025-05-10 18:46:50 +0200

    Integrated groups

--- main.py
+++ main.py
@@ -1,24 +1,40 @@
 import pygame
+from player import Player
 from constants import *
 
 def main():
     pygame.init()
-    print ("Starting Asteroids!")
-
+    print("Starting Asteroids!")
+    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+    
     clock = pygame.time.Clock()
     dt = 0
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    
+    Player.containers = (updatable, drawable)
+
+    player = Player(
+        x=SCREEN_WIDTH / 2,
+        y=SCREEN_HEIGHT / 2
+    )
+
     while True:
-        for event in pygame.event.get():
+        for event in pygame.event.get(): 
             if event.type == pygame.QUIT:
                 return
 
+        updatable.update(dt)
+        
         screen.fill("black")
-
+        
+        for entity in drawable:
+            entity.draw(screen)
+        
         pygame.display.flip()
-
+        
         dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
